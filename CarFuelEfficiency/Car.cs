c389b5322422382ffd96954bbdd131fae3781983
Ne-anna <annabusele@inbox.lv>
2023-03-05 13:59:49 +0200

    Add project files.

---
+++ CarFuelEfficiency/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFuelEfficiency
+{
+    public class Car
+    {
+        private double fuelEfficiency { get; set; }
+        private double fuelLevel { get; set; }
+
+        public Car(double efficiency)
+        {
+            fuelEfficiency = efficiency;
+            fuelLevel = 0;
+        }
+        
+        public void Drive(double distance)
+        {
+            fuelLevel -= distance / fuelEfficiency;
+            Console.WriteLine("This car has drove " + distance + " km");
+        }
+
+        public double GetFuelLevel()
+        {
+            Console.WriteLine("After driving this distance, the fuel remaining: " + fuelLevel + " litres");
+            return fuelLevel;
+        }
+
+        public void AddFuel(double amount)
+        {
+            fuelLevel += amount;
+            Console.WriteLine("This car has fuel level of " + amount + " litres");
+        }
+    }
+}
---
+++ CarFuelEfficiency/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CarFuelEfficiency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car favouriteCar = new Car(8); //8 km per litre
+            favouriteCar.AddFuel(40); //Tank with 40 litres
+            favouriteCar.Drive(200); //Driving 200 km
+            Console.WriteLine(favouriteCar.GetFuelLevel());
+        }
+    }
+}
