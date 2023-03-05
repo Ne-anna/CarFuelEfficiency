0ca159f9a09ab8becda0102b7b3e254ad468e061
Ne-anna <annabusele@inbox.lv>
2023-03-05 14:07:50 +0200

    Create moethods

--- CarFuelEfficiency/Program.cs
+++ CarFuelEfficiency/Program.cs
@@ -4,10 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Car favouriteCar = new Car(8); //8 km per litre
+            Car favouriteCar = new Car(6.5); //8 km per litre
             favouriteCar.AddFuel(40); //Tank with 40 litres
             favouriteCar.Drive(200); //Driving 200 km
-            Console.WriteLine(favouriteCar.GetFuelLevel());
+            favouriteCar.GetFuelLevel();
         }
     }
 }
